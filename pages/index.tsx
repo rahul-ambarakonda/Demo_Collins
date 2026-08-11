480c4c8eb72f3822e994b20922ce7f87593a1c3d
Rahul Ambarakonda <35566693+rahul-ambarakonda@users.noreply.github.com>
2026-08-11 12:16:26 +0530

    feat: Apply basic Tailwind class to home page for verification

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1 className="text-red-500">Hello World!</h1>
     </div>
   );
 };
